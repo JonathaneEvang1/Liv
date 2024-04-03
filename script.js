3f3a9304430f3279f6c1ef7ab4f018986882b6a0
JonathaneEvang1 <165854842+JonathaneEvang1@users.noreply.github.com>
2024-04-02 22:55:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("simBtn").addEventListener("click", function() {
+    document.getElementById("resposta").innerText = "Ufa, não tava preparado pra receber um não kk. Te amo!!!!";
+});
+
+document.getElementById("naoBtn").addEventListener("click", function() {
+    document.getElementById("resposta").innerText = "ERROR#";
+});
